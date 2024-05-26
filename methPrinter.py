a854d3a09aabca938c4166d1b4d551804c5d7294
RicardoSdV <ricardo.su.dv@gmail.com>
2024-05-26 20:38:48 +0300

    When meth wrapped print file name

--- methPrinter.py
+++ methPrinter.py
@@ -3,6 +3,9 @@ How to use:
     - Paste oMROpOCS in some high level utils type file
     - Call oMROpOCS from the callable to debug
     - Optionally set the printMRO and adjust the callStackDepth optionally at a global level or by passing the vals
+
+Known issues:
+    - Wrappers
 """
 
 import types
@@ -11,10 +14,9 @@ from inspect import stack
 from testCode import SomeClass
 
 
-def MROpOCS(pMRO=False, callStackDepth=999):
-    frames, clsMethStrs = stack()[1:callStackDepth+1], []
+def MROpOCS(pMRO=True, callStackDepth=999):
+    frames, clsMethStrs, callChain = stack()[1:callStackDepth+1], [], []
 
-    callChain = []
     for frame in frames:
         fObj, methName, = frame[0], frame[3]; fLocals = fObj.f_locals
 
@@ -22,56 +24,60 @@ def MROpOCS(pMRO=False, callStackDepth=999):
         callerCls = fLocals['self'].__class__ if isInsMeth else fLocals['cls'] if isClsMeth else None
 
         if not (isInsMeth or isClsMeth) or isinstance(callerCls, types.ClassType):
-            callChain.append(frame[1] + '.' + methName); continue
+            callChain.append(frame[1].split('/')[-1] + '.' + methName); continue
 
-        fCode, clsNs = fObj.f_code, []
+        definerClsFound, fCode, clsNs = False, fObj.f_code, []
         isPrivate = True if methName.startswith('__') and not methName.endswith('__') else False
         if isInsMeth:
             if isPrivate:
-                found = False
                 for cls in callerCls.__mro__:
                     clsNs.append(cls.__name__)
                     for attr in cls.__dict__.values():
                         if isinstance(attr, types.FunctionType) and attr.__name__ == methName and attr.func_code is fCode:
-                            found = True; break
-                    if found: break
+                            definerClsFound = True; break
+                    if definerClsFound: break
             else:
                 for cls in callerCls.__mro__:
                     clsNs.append(cls.__name__)
                     if methName in cls.__dict__:
                         method = cls.__dict__[methName]
                         if isinstance(method, property):
-                            if method.fget.func_code is fCode: break
-                        elif method.func_code is fCode: break
+                            if method.fget.func_code is fCode: definerClsFound = True; break
+                        elif method.func_code is fCode: definerClsFound = True; break
         elif isClsMeth:
             if isPrivate:
-                found = False
                 for cls in callerCls.__mro__:
                     clsNs.append(cls.__name__)
                     for attr in cls.__dict__.values():
                         if isinstance(attr, classmethod) and attr.__func__.__name__ == methName and attr.__func__.__code__ is fCode:
-                            found = True; break
-                    if found: break
+                            definerClsFound = True; break
+                    if definerClsFound: break
             else:
                 for cls in callerCls.__mro__:
                     clsNs.append(cls.__name__)
-                    if methName in cls.__dict__ and cls.__dict__[methName].__func__.__code__ is fCode: break
+                    if methName in cls.__dict__ and cls.__dict__[methName].__func__.__code__ is fCode:
+                        definerClsFound = True; break
 
+        if not definerClsFound: callChain.append(frame[1].split('/')[-1] + '.' + methName); continue
         if pMRO: clsNs[-1] = clsNs[-1] + '.' + methName + ')' * (len(clsNs) -1); callChain.append('('.join(clsNs))
         else: callChain.append(clsNs[-1] + '.' + methName)
-
     print ' <- '.join(callChain)
+
+
+def testWrapper(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
 
 
 class ITest(object):
     def testCallerOfCaller(self): raise NotImplementedError()
 class TestGanny(object): pass
 class TestDaddy(TestGanny):
+    @testWrapper
     def test(self): MROpOCS()
     def __testCaller(self): self.test()
-    def testCaller(self):
-        localVar = 1
-        self.__testCaller()
+    def testCaller(self): localVar = 1; self.__testCaller()
 class Test(TestDaddy, ITest):
     @property
     def testPropCallerOfCallerOfCaller(self): return self.testCallerOfCaller()
@@ -104,20 +110,16 @@ SomeClass().someMeth()
 
 class TestOutcastSon(TestOutcast): pass
 
-def testFunc():
-    TestOutcastSon()
+def testFunc(): TestOutcastSon()
 
 class TestOldStyle:
     @staticmethod
-    def oldStyleStaticMeth():
-        testFunc()
+    def oldStyleStaticMeth(): testFunc()
 
     @classmethod
-    def oldStyleClassMeth(cls):
-        cls.oldStyleStaticMeth()
+    def oldStyleClassMeth(cls): cls.oldStyleStaticMeth()
 
-    def oldStyleInstanceMeth(self):
-        self.oldStyleClassMeth()
+    def oldStyleInstanceMeth(self): self.oldStyleClassMeth()
 
 
 TestOldStyle().oldStyleInstanceMeth()
